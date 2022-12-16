6f8af8bcbdf7025fa43abaa8e3afd28e1622fd71
Camilo <c.acevedo1993@gmail.com>
2022-12-16 10:52:44 -0500

    fix linter error reported

--- src/middlewares/asyncHandler.ts
+++ src/middlewares/asyncHandler.ts
@@ -1,5 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
-const asyncHandler = (fn:any) => (request:Request, response: Response, next:NextFunction) => Promise.resolve(fn(request, response, next)).catch(next);
+const asyncHandler = (
+  fn:any,
+) => (
+  request:Request,
+  response: Response,
+  next:NextFunction,
+) => Promise.resolve(fn(request, response, next)).catch(next);
 
 export default asyncHandler;
